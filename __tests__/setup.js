fde6f4ffc1a2f6ed411f46aab3ab0b1f858cf936
Anna Sychugina <anna@emma-app.com>
2022-09-04 19:06:05 +0400

    fix: test setup

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,2 +1,34 @@
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.useFakeTimers();
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('react-native-track-player', () => {
+  return {
+    addEventListener: jest.fn(),
+    registerEventHandler: jest.fn(),
+    registerPlaybackService: jest.fn(),
+    setupPlayer: jest.fn(),
+    destroy: jest.fn(),
+    updateOptions: jest.fn(),
+    add: jest.fn(),
+    remove: jest.fn(),
+    skip: jest.fn(),
+    skipToNext: jest.fn(),
+    skipToPrevious: jest.fn(),
+    removeUpcomingTracks: jest.fn(),
+    reset: jest.fn(),
+    play: jest.fn(),
+    pause: jest.fn(),
+    stop: jest.fn(),
+    seekTo: jest.fn(),
+    setVolume: jest.fn(),
+    setRate: jest.fn(),
+    getQueue: jest.fn(),
+    getTrack: jest.fn(),
+    getCurrentTrack: jest.fn(),
+    getVolume: jest.fn(),
+    getDuration: jest.fn(),
+    getPosition: jest.fn(),
+    getBufferedPosition: jest.fn(),
+    getState: jest.fn(),
+    getRate: jest.fn(),
+  };
+});
